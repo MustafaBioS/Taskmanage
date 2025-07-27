d69d21798f1d6dcd30000c4b456c3c2de5289cdb
Mustafa Hany <bioshpere.voices@gmail.com>
2025-07-27 04:59:25 +0300

    vercel

--- DJANGO/settings.py
+++ DJANGO/settings.py
@@ -118,7 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_DIRS = [os.path.join(BASE_DIR, 'ToDoList/static')]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # ✅ Compatible with your setup
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
