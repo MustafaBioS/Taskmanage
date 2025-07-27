4e35fe8cd84d2a8c7add44c976fcabf0fee39277
Mustafa Hany <bioshpere.voices@gmail.com>
2025-07-27 05:51:36 +0300

    database

--- DJANGO/settings.py
+++ DJANGO/settings.py
@@ -35,11 +35,11 @@ print("ALLOWED_HOSTS =", ALLOWED_HOSTS)
 INSTALLED_APPS = [
     'ToDoList',
     'whitenoise.runserver_nostatic',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
+    # 'django.contrib.admin',
+    # 'django.contrib.auth',
+    # 'django.contrib.contenttypes',
+    # 'django.contrib.sessions',
+    # 'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
 
@@ -79,8 +79,7 @@ WSGI_APPLICATION = 'DJANGO.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.dummy',
     }
 }
 
